2181bb690bab0af028e9da098e8afe556973430b
Himanshu Sharma <sharmanshu0103@gmail.com>
2023-06-17 03:21:22 +0530

    fix: removed deprecated methods

--- app/src/main/java/com/himanshu/mvvm/data/network/NetworkConnectionInterceptor.kt
+++ app/src/main/java/com/himanshu/mvvm/data/network/NetworkConnectionInterceptor.kt
@@ -2,6 +2,9 @@ package com.himanshu.mvvm.data.network
 
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import androidx.annotation.RequiresApi
 import com.himanshu.mvvm.util.NoInternetException
 import okhttp3.Interceptor
 import okhttp3.Response
@@ -12,6 +15,7 @@ class NetworkConnectionInterceptor(
 
     private val applicationContext = context.applicationContext
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun intercept(chain: Interceptor.Chain): Response {
         if(!isInternetAvailable()){
             throw NoInternetException("No Internect Connection")
@@ -19,10 +23,15 @@ class NetworkConnectionInterceptor(
         return chain.proceed(chain.request())
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     private fun isInternetAvailable() : Boolean{
         val connectivityManager = applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        connectivityManager.activeNetworkInfo.also {
-            return it != null && it.isConnected
+            val network = connectivityManager.activeNetwork ?: return false
+            val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
+            return when {
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                else -> false
         }
     }
 }
